a18a454986d87799d22a0af027899f1ede212ba6
fahmanfaisal <fahmanchy@gmail.com>
2026-09-02 00:03:09 +0600

    added-functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import { Header } from "./components/Header";
 import { HabitForm } from "./components/HabitForm";
 import { HabitList } from "./components/HabitList";
+import { useState } from "react";
+import type { Habit } from "./components/HabitList";
+import { isSameDay } from "date-fns";
 export default function App() {
+
+  const [habits, setHabits] = useState<Habit[]>([]);
+  
+  function addHabit(name: string) {
+    setHabits([...habits, { id: habits.length + 1, name, completions: [new Date()] }]);
+  }
+
+  function removeHabit(id: string) {
+    setHabits(habits.filter(habit => habit.id.toString() !== id));
+  }
+
+  function toggleHabit(id: string, date: Date) {
+    setHabits(habits.map(habit => {
+      if (habit.id.toString() === id) {
+        const isCompleted = habit.completions.some(d => isSameDay(d, date));
+        return {
+          ...habit,
+          completions: isCompleted
+            ? habit.completions.filter(d => !isSameDay(d, date))
+            : [...habit.completions, date]
+        };
+      }
+      return habit;
+    }));
+  }
+
   return (
     <div className="max-w-2xl mx-auto flex flex-col gap-4 p-4">
       <Header />
-      <HabitForm />
-      <HabitList />
+      <HabitForm addHabit={addHabit} />
+      <HabitList habits={habits} removeHabit={removeHabit} toggleHabit={toggleHabit} />
     </div>
   );
 }
--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,10 +1,29 @@
 import { Button } from "./button";
+import { useState } from "react";
 
-export function HabitForm() {
+type HabitFormProps = {
+    addHabit: (name: string) => void;
+};
+
+export function HabitForm({ addHabit} : HabitFormProps) {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e: React.SubmitEvent) {
+    e.preventDefault();
+
+    if  (name.trim() === "") return;
+    setName("");
+    addHabit(name);
+  }
+
   return (
-    <form className="flex flex-row items-center gap-2">
-      <input type="text" placeholder="New Habit" className="flex-1 bg-zinc-800 px-4 py-2 rounded-lg text-white outline-none focus:visible:ring-2 focus-visible:to-blue-400 placeholder:text-zinc-500 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-      <Button variant="primary" className="bg-blue-900 px-4 py-2 font-medium">Add Habit</Button>
+    <form className="flex flex-row items-center gap-2" onSubmit={handleSubmit}>
+      <input type="text" placeholder="New Habit" value={name} onChange={(e) => setName(e.target.value)} className="flex-1 bg-zinc-800 px-4 py-2 rounded-lg text-white outline-none focus:visible:ring-2 focus-visible:to-blue-400 placeholder:text-zinc-500 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+      <Button disabled={name.trim() === ""}
+       variant="primary"
+       className="bg-blue-900 px-4 py-2 font-medium">
+        Add Habit
+      </Button>
     </form>
   );
 }
